9539733ce620c608233977dcc4712de7fc25098e
fixnix <fixnixdev@gmail.com>
2013-08-30 11:27:09 +0530

    flipbook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Website::Application.config.secret_key_base = '9273b1ad630f270cc13e960266cac396a7351346c7dd83bf070e9003f1ebd439e5e65024bb1ed5a92ad97761c44a2a53179d4a1af674d10fb0c599eb903357d8'
